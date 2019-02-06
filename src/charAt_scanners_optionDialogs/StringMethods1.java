368ddc1deb3277e2c4df24b2c4775bb2770f85fa
knowdawg <>
2019-02-05 19:53:48 -0800

    stuff
    Pi Aloud
    String Methods 1
    Change Calculator

--- src/charAt_scanners_optionDialogs/StringMethods1.java
+++ src/charAt_scanners_optionDialogs/StringMethods1.java
@@ -8,16 +8,18 @@ public class StringMethods1 {
 	public static void main(String[] args) {
 		
 		// 1. Create a String variable and initialize it to whatever you want
-
+String s = "hello";
 		// 2. Print the 3rd char of your String to the console.
 		//    HINT: .charAt
-		
+System.out.println(s.charAt(3));	
 		// 3. Print the length of your String to the console.
 		//    HINT: .length()
-
+System.out.println(s.charAt(s.length()-1));
 		// 4. Using a for loop, print one char at time to the console.
 		//    HINT: use .length() to determine how many loops
-
+for(int i = 0;i<s.length();i++) {
+	System.out.println(s.charAt(i));
+}
 		
 		// 5. Pick a char inside your String, and use a loop to determine
 		//    what position/index in the String the char is located.
@@ -25,6 +27,11 @@ public class StringMethods1 {
 		//    EXAMPLE: if your string is "abc" and you are searching 
 		//             for char 'b', then print "b is at index 1"
 		
+for(int ii = 0; ii<s.length(); ii++) {
+			if(s.charAt(ii)=='l'){
+				System.out.println("l is at index " + ii);
+			}
+		}
 	}
 }
 
--- src/doubles_and_booleans/ChangeCalculator.java
+++ src/doubles_and_booleans/ChangeCalculator.java
@@ -15,17 +15,22 @@ public class ChangeCalculator {
 	public static void main(String[] args) {
 
 		// Ask the user how many nickels they have
-
+String nn = JOptionPane.showInputDialog("How many nickels do you have?");
 		// Convert their answer to an int using Integer.parseInt()
-
+int n = Integer.parseInt(nn);
 		// Ask the user how many dimes they have, and convert their answer
-
+String dd = JOptionPane.showInputDialog("How many dimes do you have?");
+int d = Integer.parseInt(dd);
 		// Ask the user how many quarters they have, and convert their answer
-
-		// Calculate how much money the user has and save it in a double variable 
-
+String qq = JOptionPane.showInputDialog("How many quarters do you have?");
+int q = Integer.parseInt(qq);
+	// Calculate how much money the user has and save it in a double variable 
+double money = 0.0;
+money += n*.05;
+money += d*.10;
+money += q*.25;
 		// Tell the user how much money they have
-
+JOptionPane.showMessageDialog(null, "You have " + money + " dollars");
 	}
 }
 
