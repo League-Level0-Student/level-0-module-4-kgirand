b8289a35106343415e804f953d487810426ab208
kgirand <league@WTS20.attlocal.net>
2019-02-19 19:55:23 -0800

    etch
    hdfshhg

--- src/extra/StringMethods2.java
+++ src/extra/StringMethods2.java
@@ -8,18 +8,19 @@ public class StringMethods2 {
 		
 		// 1. Create a String variable and initialize it with
 		//    random upper and lower case characters.
-
+String s ="apple";
 		// 2. Print your String to the console in upper case.
-		
+		System.out.println(s.toUpperCase());
 		// 3. Print your String to the console in lower case.
-
+		System.out.println(s.toLowerCase());
 		// 4. Print the first 3 char's of your String
 		//    HINT: .substring(start,end)
-		
+		System.out.println(s.substring(0,3));
 		// 5. Print a single char somewhere in the middle of your String
 		//    REMINDER: char's in string start at index 0
-		
+		System.out.println(s.charAt(3));
 		// 6. BONUS -- print the LAST 3 char's of your string using
 		//        .length() to determine WHERE the last 3 char's are located.
+		System.out.println(s.substring(2));
 	}
 }
